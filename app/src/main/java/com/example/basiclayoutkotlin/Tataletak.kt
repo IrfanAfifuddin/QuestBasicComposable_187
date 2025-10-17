3281c37d0df4429d1768f531b821d485615e7d47
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-10-17 10:11:26 +0700

    add Column text

--- app/src/main/java/com/example/basiclayoutkotlin/Tataletak.kt
+++ app/src/main/java/com/example/basiclayoutkotlin/Tataletak.kt
@@ -1,5 +1,14 @@
 package com.example.basiclayoutkotlin
 
-fun TataletakColumn() {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
 
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
 }
